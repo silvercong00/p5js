ca9963b6fa412913a0556d3825befb741fcc532e
silvercong00 <dldmsqls423@gmail.com>
2025-11-27 22:59:24 +0900

    Add files via upload

---
+++ 과제2/sketch.js
@@ -0,0 +1,14 @@
+let img;
+
+function preload() {
+  img = loadImage("project2.png");
+}
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(255);
+  image(img, 0, 0, width, height);
+}
